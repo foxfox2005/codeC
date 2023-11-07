3d15b7e17c2431ab9ba2e5747027f7c54bb640b3
foxfox2005 <minhhieuanlao1970@gmail.com>
2023-11-07 02:49:02 -0800

    c

--- test.cpp
+++ test.cpp
@@ -1,5 +1,15 @@
 #include <cstdio>
 
 int main () {
-    printf ("hell nah");
+    float num;
+    printf("Nhap so diem tu 1 den 10:");
+    scanf("%f, &num");
+    if(num < 0 || num >=10) {
+        printf("nhap dung so diem tren thang diem 10");
+    } else if (num >0 &&  num < 3,5) {
+        printf("hoc luc kem");
+    } else if (num >= 3,5 && num < 5) {
+        printf("hoc luc yeu");
+    }
+    return 0;
 }
