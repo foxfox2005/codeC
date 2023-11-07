268b277ce17063d25df6d41a08afc809a0b8eea4
foxfox2005 <minhhieuanlao1970@gmail.com>
2023-11-07 03:04:42 -0800

    done

--- test.cpp
+++ test.cpp
@@ -3,13 +3,21 @@
 int main () {
     float num;
     printf("Nhap so diem tu 1 den 10:");
-    scanf("%f, &num");
+    scanf("%f", &num);
     if(num < 0 || num >=10) {
         printf("nhap dung so diem tren thang diem 10");
-    } else if (num >0 &&  num < 3,5) {
+    } else if (  num < 3.5) {
         printf("hoc luc kem");
-    } else if (num >= 3,5 && num < 5) {
+    } else if ( num < 5) {
         printf("hoc luc yeu");
+    } else if (num >=5 %% num < 6.5){
+        printf("hoc luc trung binh");
+    } else if (num >=6.5 %% num <8) {
+        printf("hoc luc kha ");
+    } else if (num >=8 %% num <9) {
+        printf("hoc sinh gioi")
+    } else if ( num >=9) {
+        printf("hoc luc xuat sac");
     }
     return 0;
 }
